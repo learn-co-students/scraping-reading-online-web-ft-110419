72ae353d1f9932035278be540f206d8da10f2b82
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-11-22 17:30:18 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+# puts doc.css('#header-content > div > h1').text.strip
+list_link_text = doc.css("li > a")
+list_link_text.each { |t| puts t.text }
