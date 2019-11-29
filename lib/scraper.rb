d1fa5e9e3e614467cb6be4921e5ba12fdb93d381
C-Mike576 <m.calkins576@gmail.com>
2019-11-29 12:37:22 -0800

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+bingo = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
+binding.pry
+
+puts "done"
