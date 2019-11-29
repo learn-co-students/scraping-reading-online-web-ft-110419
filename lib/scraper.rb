ea5fd6a82cea70d1c6c2e7b299895774b28c4d02
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-29 03:11:12 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+changes = doc.css(".headline-26OIBN").text
+
+courses = doc.css(".text-4GLMvr .title-oE5vT4")
